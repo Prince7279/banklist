5c9cd56ddeaedea5a58f5571b4c8ff0210ce5b0f
Bisha4225 <kumarbishal4225@gmail.com>
2023-01-31 22:22:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const currencies = new Map([
+  ['USD', 'United State Dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound stering'],
+]);
+const movement = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+console.log(arr.slice(2));
